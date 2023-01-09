2286493b34ce7628cb229bdf86b855b3a0eae007
ibrahim <ibrahimk4094@gmail.com>
2023-01-09 12:31:29 +0300

    exercieses and integer comparaision added

---
+++ 033_exercises.sh
@@ -0,0 +1,14 @@
+#! /bin/bash
+# read -p "Enter two number" num1 num2
+
+#let total=$num1+$num2
+
+#echo "Total: $total"
+
+#echo "sum of numbers $(( num1 + num2 ))"
+
+#/**/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/
+
+#echo "sum of numbers $(( $1 + $2 ))"
+let total=$1+$2
+echo "Total: $total"
